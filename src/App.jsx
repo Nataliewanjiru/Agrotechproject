0fcd2f1b6b16d9264b77c67d7c20ee937993b18f
Ian Ndung'u <ianndungu06@gmail.com>
2023-10-05 00:09:49 +0300

    done with crops component and added the carousel in the livestock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Livestock from './livestocks'; 
+import Crop from './crop';
 
 const App = () => {
   return (
     <div className="app">
       <Livestock />
+      <Crop />
     </div>
   );
 }
--- src/livestocks.jsx
+++ src/livestocks.jsx
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
-import livestockcss from './livestocks.css';
+import  './livestocks.css';
+import  Carousel from "./carousel";
+
 
 function Livestock() {
   const [livestockType, setLivestockType] = useState("");
   const [weaningDate, setWeaningDate] = useState("");
   const [slaughterDate, setSlaughterDate] = useState("");
   const [quantity, setQuantity] = useState("");
+  const [livestocks, setLivestocks] = useState([]);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log({
+    const newLivestock = {
       livestockType,
       weaningDate,
       slaughterDate,
       quantity
-    });
+    };
+    setLivestocks([...livestocks, newLivestock]);
+    setLivestockType("");
+    setWeaningDate("");
+    setSlaughterDate("");
+    setQuantity("");
   };
 
   return (
-        <div className="main" style={{ fontFamily: 'Arial, sans-serif', maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
-          <main>
-            <h1 style={{ fontSize: '2em', color: '#333' }}>Livestock Management</h1>
+    <div className="main" style={{ fontFamily: 'Arial, sans-serif', maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
+        <h1 style={{ fontSize: '2em', color: '#333' }}>Livestock Management</h1>
+        <div className="carousel">
+        <Carousel/>
+        </div>
+        <main>
         <section>
-          <div className="">
+          <div className="Text">
             <p>
               Welcome to a world where livestock aren't just animals; they're
               the heartbeat of a sustainable and thriving ecosystem. Together,
@@ -41,7 +52,7 @@ function Livestock() {
             <p>
               Explore our collection of resources that help you manage your
               livestock records including: Livestock type, Weaning date,
-              Slaughter date, Quantity{" "}
+              Slaughter date, Quantity.{" "}
             </p>
           </div>
         </section>
@@ -85,24 +96,40 @@ function Livestock() {
             </div>
             <button type="submit">Add Livestock</button>
           </form>
+          <div className="added-livestocks">
+            <h2>Added Livestocks</h2>
+            <ul>
+              {livestocks.map((livestock, index) => (
+                <li key={index}>
+                  Type: {livestock.livestockType}, Weaning Date: {livestock.weaningDate}, Slaughter Date: {livestock.slaughterDate}, Quantity: {livestock.quantity}
+                </li>
+              ))}
+            </ul>
+          </div>
         </section>
         <section>
           <div className="livestock-images">
+            <div className="Cow">
             <img src="https://i.pinimg.com/564x/b1/1b/20/b11b20effe9cd6537b4491ada98f44b5.jpg" alt="Holstein-Friesian"/>
             <p>
               Holstein-Friesian
               Advantages: Known for the highest milk production among all dairy breeds. They are well-suited for intensive dairy farming.
             </p>
+            </div>
+            <div className="Rabbit">
             <img src="https://i.pinimg.com/564x/4b/7d/60/4b7d60249acad9a7dda997d1b024ae8a.jpg" alt="New Zealand White"/>
             <p>
               New Zealand White
               Advantages: Known for their rapid growth, high meat-to-bone ratio, and good feed conversion. They have a calm temperament.
             </p>
+            </div>
+            <div className="Chicken">
             <img src="https://i.pinimg.com/564x/11/76/0c/11760c22517d44d14d6d029619517403.jpg" alt="Rhode Island Red"/>
             <p>
               Rhode Island Red
               Advantages: Known for excellent egg production and hardiness. They are good for backyard flocks and small farms.
             </p>
+            </div>
           </div>
         </section>
       </main>
