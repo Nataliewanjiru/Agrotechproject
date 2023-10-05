4e1cf0e93345cdbb5dd8c76b28ad623a10ae7603
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-10-05 13:45:22 +0300

    Update to user working

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,34 +1,87 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Register = ({ changeForm }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [firstname, setfirstName]= useState("")
-    const[lastname, setlastName] = useState("")
-    const[role, setRole] = useState("")
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        role: "",
+    });
 
-    const signUp = (e) => {
+    const { first_name, last_name, email, password, role } = formData;
+
+    const signUp = async (e) => {
         e.preventDefault();
+        console.log(formData);
 
-        setEmail("")
-        setPassword("")
-        setfirstName("")
-        setlastName("")
-        setRole("")
+        try {
+            const response = await axios.post("https://agrotechbackend.onrender.com/user", formData);
+            console.log(response.data); // Handle the response from the backend
+        } catch (error) {
+            console.error(error);
+        }
+
+        // Clear the form inputs after submission
+        setFormData({
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: "",
+            role: "",
+        });
     };
 
     return (
         <div className="sign-in-container">
             <form onSubmit={signUp}>
                 <h1>Create Account</h1>
-                <input type="text" placeholder="Enter your first name.." value={firstname} onChange={(e) => setfirstName(e.target.value)} ></input>
-                <input type="text" placeholder="Enter your last name.." value={lastname} onChange={(e) => setlastName(e.target.value)} ></input>
-                <input type="email" placeholder="Enter your email.." value={email} onChange={(e) => setEmail(e.target.value)} ></input>
-                <input type="password" placeholder="Enter your password" value={password}onChange={(e) => setPassword(e.target.value)}></input>
-                <input type="text" placeholder="Enter your role (Advisor/Farmer)" value={role} onChange={(e) => setRole(e.target.value)} ></input>
+                <input
+                    type="text"
+                    placeholder="Enter your first name.."
+                    value={first_name}
+                    onChange={(e) =>
+                        setFormData({ ...formData, first_name: e.target.value })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="Enter your last name.."
+                    value={last_name}
+                    onChange={(e) =>
+                        setFormData({ ...formData, last_name: e.target.value })
+                    }
+                />
+                <input
+                    type="email"
+                    placeholder="Enter your email.."
+                    value={email}
+                    onChange={(e) =>
+                        setFormData({ ...formData, email: e.target.value })
+                    }
+                />
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) =>
+                        setFormData({ ...formData, password: e.target.value })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="Enter your role (Advisor/Farmer)"
+                    value={role}
+                    onChange={(e) =>
+                        setFormData({ ...formData, role: e.target.value })
+                    }
+                />
                 <button type="submit">Sign Up</button>
             </form>
-            <button onClick={() => changeForm("login")}>Already have an account.</button>
+            <button onClick={() => changeForm("login")}>
+                Already have an account.
+            </button>
         </div>
     );
 };
