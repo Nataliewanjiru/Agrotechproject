828ebe4476c3155d6908c43f97864bd857055ca9
Kamente <justinm48000@gmail.com>
2023-10-05 21:25:35 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+// import Finance from './Components/Finance';
+import './App.css';
+import Equipment from './Components/Equipments';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <h1>Equipment Data</h1>
+        {/* <Finance /> */}
+        {<Equipment />}
+      </header>
+    </div >
+  );
 }
 
-export default App
+export default App;
